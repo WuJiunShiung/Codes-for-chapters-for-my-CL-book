93e4bc0e6543067d3e32a8603a43b7c2013d0aec
WuJiunShiung <WuJiunShiung@users.noreply.github.com>
2015-04-24 21:41:16 +0800

    Create family_Carl.pl

---
+++ family_Carl.pl
@@ -0,0 +1,16 @@
+% File family_Carl.pl
+% the sons of Carl.
+/* father(x, y) means x is 
+y's father */
+
+
+father(carl, paul).
+father(carl, patrick).
+father(carl, peter).
+father(carl, philip).
+father(carl, phoebe).
+father(philip, tom).
+father(philip, tommy).
+father(jim, david).
+father(jim, bill).
+father(jim, sam).
